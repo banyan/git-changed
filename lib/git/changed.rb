ec3bac2ad29f1580c993ff496a2381cf2e1ae2bf
Kohei Hasegawa <ameutau@gmail.com>
2015-03-08 22:54:29 -0400

    Show modified files of working tree if no argument is passed

--- lib/git/changed.rb
+++ lib/git/changed.rb
@@ -1,7 +1,12 @@
 require "git/changed/version"
+require 'contracts'
 
 module Git
   module Changed
+    include Contracts
+    include Contracts::Modules
+
+    Contract Maybe[String] => nil
     def self.run
       case ARGV.first
       when '-v', '--version'
@@ -11,12 +16,27 @@ module Git
       end
     end
 
+    Contract String => nil
     def self.version
       puts Git::Changed::VERSION
     end
 
+    Contract Maybe[String] => nil
     def self.changed(hash = nil)
-      hash = latest_hash if hash.nil?
+      if hash.nil?
+        show_modified_files_on_working_tree
+      else
+        show_modified_files_on_commit hash
+      end
+    end
+
+    Contract nil => nil
+    def self.show_modified_files_on_working_tree
+      puts `git status --porcelain`.each_line.map { |x| x[3..-1] }
+    end
+
+    Contract Maybe[String] => nil
+    def self.show_modified_files_on_commit(hash)
       hashes = `git show --summary --format="%P" #{hash} | head -n 1`.split ' '
 
       if hashes.size == 2
@@ -26,6 +46,7 @@ module Git
       end
     end
 
+    Contract nil => String
     def self.latest_hash
       `git log --pretty=format:'%h' -n 1`
     end
--- spec/git/changed_spec.rb
+++ spec/git/changed_spec.rb
@@ -17,20 +17,32 @@ describe Git::Changed do
   end
 
   describe '.changed' do
-    context 'normal commit' do
-      context 'when argument is passed' do
-        it "outputs modified files to stdout" do
+    context 'when argument is not passed' do
+      context 'when there are no modified files in working trees' do
+        it "does not output modified files to stdout" do
           first_commit do
-            expect{ Git::Changed.changed(Git::Changed.latest_hash) }.to output(/a\.txt/).to_stdout
+            expect{ Git::Changed.changed }.to output('').to_stdout
           end
         end
       end
 
-      context 'when argument is not passed' do
+      context 'when there are modified files in working trees' do
         it "outputs modified files to stdout" do
           first_commit do
-            expect{ Git::Changed.changed }.to output(/a\.txt/).to_stdout
+            `touch b.txt`
+            `git add .`
+            `touch c.txt`
+
+            expect{ Git::Changed.changed }.to output(/b\.txt\nc\.txt/).to_stdout
           end
+        end
+      end
+    end
+
+    context 'when argument is passed' do
+      it "outputs modified files to stdout" do
+        first_commit do
+          expect{ Git::Changed.changed(Git::Changed.latest_hash) }.to output(/a\.txt/).to_stdout
         end
       end
     end
@@ -53,7 +65,7 @@ describe Git::Changed do
           `git merge --no-ff c-txt` # Add no-ff option to immitate GitHub pull requests
           `git merge --no-ff b-txt`
 
-          expect{ Git::Changed.changed }.to output(/b\.txt/).to_stdout
+          expect{ Git::Changed.changed(Git::Changed.latest_hash) }.to output(/b\.txt/).to_stdout
         end
       end
     end
